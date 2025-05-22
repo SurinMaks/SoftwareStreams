73657e182896bf926b79a29780a45482939f19c1
Maxim Surin <surin.maks@yandex.ru>
2025-05-22 11:05:20 +0300

    update cmake + main

--- main.cpp
+++ main.cpp
@@ -1,5 +1,10 @@
 #include <iostream>
+#include <libssh/libssh.h>
+#include <memory>
+#include "Connect.hpp"
 
 int main(int, char**){
     std::cout << "Hello, from streams!\n";
+    std::unique_ptr<SS::IConnect> myssh = std::make_unique<SS::Connect>("127.0.0.1", 123);
+    myssh->create();
 }

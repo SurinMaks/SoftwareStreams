809773060932f983f7edf9cc8072cf5df968b074
Maxim Surin <surin.maks@yandex.ru>
2025-05-22 11:42:42 +0300

    исправил типы, добавил библиотеку libssh

--- src/Connect.cpp
+++ src/Connect.cpp
@@ -1,12 +1,13 @@
 #include <iostream>
+#include <libssh/libssh.h>
 #include "Connect.hpp"
 
 namespace SS
 {
-    Connect::Connect(const std::string& host, const u_int16_t port)
-        : host_(host), port_(port) {};
+    SS::Connect::Connect(const std::string& host, const uint16_t port)
+        : host_(host), port_(port) {}
 
     void SS::Connect::create(){
         std::cout << "Method create ... \n";
-    };
+    }
 } // namespace SS
